511c1debc8c28e2699318ae57556d93219e71d3c
SORNON <sornon0719@gmail.com>
2025-09-26 12:13:43 +0800

    Fix safe-area spacing for custom navigation

---
+++ miniprogram/components/custom-nav/custom-nav.js
@@ -0,0 +1,65 @@
+const app = getApp();
+
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  properties: {
+    title: {
+      type: String,
+      value: ''
+    },
+    enableBack: {
+      type: Boolean,
+      value: true
+    },
+    theme: {
+      type: String,
+      value: 'light'
+    }
+  },
+
+  data: {
+    statusBarHeight: 0,
+    navHeight: 64,
+    navBarHeight: 44,
+    navPlaceholderHeight: 64,
+    showBack: false,
+    canNavigateBack: false
+  },
+
+  lifetimes: {
+    attached() {
+      const { customNav = {}, safeArea = {} } = app.globalData || {};
+      const statusBarHeight = customNav.statusBarHeight ?? safeArea.top ?? 0;
+      const navHeight = customNav.navHeight || (statusBarHeight + 44);
+      const navBarHeight = navHeight - statusBarHeight;
+      const navPlaceholderHeight = navHeight > 0 ? navHeight : navBarHeight;
+      const pages = getCurrentPages();
+      const canNavigateBack = pages.length > 1;
+      const showBack = !!this.data.enableBack;
+
+      this.setData({
+        statusBarHeight,
+        navHeight,
+        navBarHeight: navBarHeight > 0 ? navBarHeight : 44,
+        navPlaceholderHeight: navPlaceholderHeight > 0 ? navPlaceholderHeight : 44,
+        showBack,
+        canNavigateBack
+      });
+    }
+  },
+
+  methods: {
+    handleBack() {
+      const pages = getCurrentPages();
+      const canNavigateBack = pages.length > 1 || this.data.canNavigateBack;
+      if (canNavigateBack) {
+        wx.navigateBack({ delta: 1 });
+      } else {
+        wx.reLaunch({ url: '/pages/index/index' });
+      }
+    }
+  }
+});
